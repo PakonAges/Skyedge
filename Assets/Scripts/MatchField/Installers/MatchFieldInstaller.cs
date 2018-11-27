466b72219c2000b493b50b8d0f669ac593092c25
Max Okorokov <maxim.okorokov@gmail.com>
2018-11-27 18:22:39 +0200

    Moved to Unity 2018.3

--- Assets/Scripts/MatchField/Installers/MatchFieldInstaller.cs
+++ Assets/Scripts/MatchField/Installers/MatchFieldInstaller.cs
@@ -1,13 +1,25 @@
-﻿using Zenject;
+﻿using System;
+using UnityEngine;
+using Zenject;
 
 public class MatchFieldInstaller : MonoInstaller
 {
+    public GameObject FieldVisualizatonSetup;
+
     public override void InstallBindings()
     {
         Container.Bind<IFieldSceneController>().To<DefaultFieldSceneController>().WhenInjectedInto<HotKeyInput>();
         Container.Bind<IFieldGenerator>().To<FieldGenerator>().AsSingle();
         Container.Bind<IFieldDataProvider>().To<FieldDataProvider>().AsSingle();
         Container.Bind<IFieldVisualization>().To<DefaultFieldVisualization>().AsSingle();
+
+        InstallVisualization();
+    }
+    private void InstallVisualization()
+    {
+        Container.Bind<IFieldItemWorldPositionProvider>().To<FieldItemWorldPositionProvider>().WhenInjectedInto<DefaultFieldVisualization>();
+        Container.Bind<IFieldItemVisualProvider>().To<FieldItemVisualProvider>().WhenInjectedInto<DefaultFieldVisualization>();
+        Container.Bind<IFieldItemSpawner>().To<FieldItemSpawner>().WhenInjectedInto<DefaultFieldVisualization>();
     }
 
     //void InstallNullObjects()
