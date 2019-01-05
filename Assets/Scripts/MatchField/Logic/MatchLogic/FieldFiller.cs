ec350537b84710c49bd0e7eb33c3d4fbfdf62df1
Max Okorokov <maxim.okorokov@gmail.com>
2019-01-05 18:11:25 +0200

    All sides movement supported

--- Assets/Scripts/MatchField/Logic/MatchLogic/FieldFiller.cs
+++ Assets/Scripts/MatchField/Logic/MatchLogic/FieldFiller.cs
@@ -29,7 +29,7 @@ public class FieldFiller : IFieldFiller
     async Task<bool> FillStepAsync()
     {
         bool movedChip = false;
-        FillDirection = FieldFillDirection.TopToBot; //Debuging!
+        FillDirection = FieldFillDirection.RightToLeft; //Debuging!
 
         //Point to refactor sometime
         switch (FillDirection)
@@ -53,7 +53,6 @@ public class FieldFiller : IFieldFiller
                             GameField.FieldMatrix[x, y + 1] = chip;
 
                             GameField.FieldMatrix[x, y] = _chipManager.SpawnEmptyChip(x, y);
-                            //await _chipMovement.Swap(chip, chipBelow);
                             movedChip = true;
                         }
                     }
@@ -76,13 +75,131 @@ public class FieldFiller : IFieldFiller
             }
             break;
 
+
             case FieldFillDirection.BotToTop:
+            for (int y = 1; y < GameField.Ysize ; y++)
+            {
+                for (int x = 0; x < GameField.Xsize; x++)
+                {
+                    Chip chip = GameField.FieldMatrix[x, y];
+
+                    if (chip.IsMovable)
+                    {
+                        Chip chipAbove = GameField.FieldMatrix[x, y - 1];
+
+                        if (chipAbove.ChipType == ChipType.EmptyCell)
+                        {
+                            _chipManager.RemoveChip(chipAbove); //!
+
+                            await _chipMovement.MoveAsync(chip, x, y - 1);
+                            GameField.FieldMatrix[x, y - 1] = chip;
+
+                            GameField.FieldMatrix[x, y] = _chipManager.SpawnEmptyChip(x, y);
+                            movedChip = true;
+                        }
+                    }
+                }
+            }
+
+            //Check Bot Row
+            for (int x = 0; x < GameField.Xsize; x++)
+            {
+                Chip BotRowChip = GameField.FieldMatrix[x, GameField.Ysize - 1];
+
+                if (BotRowChip.ChipType == ChipType.EmptyCell)
+                {
+                    _chipManager.RemoveChip(BotRowChip);
+                    var newChip = await _chipManager.SpawnRandomChip(x, GameField.Ysize);
+                    await _chipMovement.MoveAsync(newChip, x, GameField.Ysize - 1);
+                    GameField.FieldMatrix[x, GameField.Ysize - 1] = newChip;
+                    movedChip = true;
+                }
+            }
             break;
+
 
             case FieldFillDirection.LeftToRight:
+            for (int x = GameField.Xsize - 2; x >= 0; x--)
+            {
+                for (int y = 0; y < GameField.Ysize; y++)
+                {
+                    Chip chip = GameField.FieldMatrix[x, y];
+
+                    if (chip.IsMovable)
+                    {
+                        Chip chipToTheRight = GameField.FieldMatrix[x +1, y];
+
+                        if (chipToTheRight.ChipType == ChipType.EmptyCell)
+                        {
+                            _chipManager.RemoveChip(chipToTheRight);
+
+                            await _chipMovement.MoveAsync(chip, x + 1, y);
+                            GameField.FieldMatrix[x + 1, y] = chip;
+
+                            GameField.FieldMatrix[x, y] = _chipManager.SpawnEmptyChip(x, y);
+                            movedChip = true;
+                        }
+                    }
+                }
+            }
+
+            //Check Left Column
+            for (int y = 0; y < GameField.Ysize; y++)
+            {
+                Chip LeftColumnChip = GameField.FieldMatrix[0, y];
+
+                if (LeftColumnChip.ChipType == ChipType.EmptyCell)
+                {
+                    _chipManager.RemoveChip(LeftColumnChip);
+                    var newChip = await _chipManager.SpawnRandomChip(-1, y);
+                    await _chipMovement.MoveAsync(newChip, 0, y);
+                    GameField.FieldMatrix[0, y] = newChip;
+                    movedChip = true;
+                }
+            }
             break;
 
+
+
             case FieldFillDirection.RightToLeft:
+            for (int x = 1; x < GameField.Xsize; x++)
+            {
+                for (int y = 0; y < GameField.Ysize; y++)
+                {
+                    Chip chip = GameField.FieldMatrix[x, y];
+
+                    if (chip.IsMovable)
+                    {
+                        Chip chipToTheLeft = GameField.FieldMatrix[x - 1, y];
+
+                        if (chipToTheLeft.ChipType == ChipType.EmptyCell)
+                        {
+                            _chipManager.RemoveChip(chipToTheLeft);
+
+                            await _chipMovement.MoveAsync(chip, x - 1, y);
+                            GameField.FieldMatrix[x - 1, y] = chip;
+
+                            GameField.FieldMatrix[x, y] = _chipManager.SpawnEmptyChip(x, y);
+                            movedChip = true;
+                        }
+                    }
+                }
+            }
+
+            //Check Right Column
+            for (int y = 0; y < GameField.Ysize; y++)
+            {
+                Chip RightColumnChip = GameField.FieldMatrix[GameField.Xsize - 1, y];
+
+                if (RightColumnChip.ChipType == ChipType.EmptyCell)
+                {
+                    _chipManager.RemoveChip(RightColumnChip);
+                    var newChip = await _chipManager.SpawnRandomChip(GameField.Xsize, y);
+                    await _chipMovement.MoveAsync(newChip, GameField.Xsize - 1, y);
+                    GameField.FieldMatrix[GameField.Xsize - 1, y] = newChip;
+                    movedChip = true;
+                }
+            }
             break;
 
             default:
