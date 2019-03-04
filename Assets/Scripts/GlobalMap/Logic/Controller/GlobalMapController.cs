7eabc06f153fa09728ef1c6346ed8d4f6f70fc05
Max Okorokov <maxim.okorokov@gmail.com>
2019-03-05 01:03:32 +0200

    todo Notes on Global Map added

--- Assets/Scripts/GlobalMap/Logic/Controller/GlobalMapController.cs
+++ Assets/Scripts/GlobalMap/Logic/Controller/GlobalMapController.cs
@@ -12,11 +12,21 @@
         public void InitGlobalMap()
         {
             _uIController.ShowGlobalMapHUDAsync();
+            //Spawn Hero -> Position from save
+            //In Region or Traveling
+            //Calculate How far did he go, when I was online
+            //Focus Camera On Hero
+            //If Trying to Move Camera -> stop focusing
+            
+            //On Region Click -> Show Region Info
+            //If Hero is not moving -> Can select Region to start movement
+            //Start Movement -> Hero moving and time elapsed
+            //On finish movement -> Can Enter Region
         }
 
         public void EnterRegion()
         {
-
+            
         }
     }
 }
